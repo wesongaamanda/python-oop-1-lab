e70cab379dafaa00f39cc7becb2e48d453ce2d71
wesongaamanda <wesongaamanda@gmail.com>
2026-06-06 15:50:12 +0300

    add

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,27 @@
-#!/usr/bin/env python3
-
 class Coffee:
-    pass
+    """Represents a coffee with a size and price."""
+
+    VALID_SIZES = ["Small", "Medium", "Large"]
+
+    def __init__(self, size, price):
+        
+        self.size = size
+       
+        self.price = price
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        
+        if value not in self.VALID_SIZES:
+            print("size must be Small, Medium, or Large")
+        else:
+            self._size = value
+
+    def tip(self):
+        """Prints a tip message and increases the price by 1."""
+        print("This coffee is great, here's a tip!")
+        self.price += 1
